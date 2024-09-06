1eb8836c18d93e53cf46e3f2a8d954e96af3230e
hyunho1111 <2mjkhh@gmail.com>
2024-09-06 17:26:17 +0900

    리액트 1일차

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
 function App() {
-  return (
-    <div>hello world</div>
-  );
+  // logic
+  console.log("app.js");
+
+  const name = "김현호";
+  const age = 34;
+
+  const greetingmessage = `안녕하세요.${name}님 나이가${age}세 이시네요.
+환영합니다!`
+  console.log("🚀 ~ App ~ greetingmessage:", greetingmessage)
+
+
+  // 단축 평가 논리 계산법 logical operators
+  const user = {
+    isLoggedIn: true,
+    role: "admin", // 'guest', 'user'
+  };
+
+  // 사용자가 로그인 한 상태이면서, 역할이 'admin'인 경우엔 "관리자가 페이지 접근 가능", 아니면" 관리자 페이지 접근 불가능"
+
+  if (!user.isLoggedIn || user.role !== "admin") {
+    console.log("관리자 페이지 접근 가능");
+  } else {
+    console.log("관리자 페이지 접근 불가능!!")
+  }
+  // view
+  return <div>hello world</div>;
 }
+
+
 
 export default App;
